23efecc68d06549ee5ddf4264b8d0c0e94416167
ChristelleM28 <christellemonvoisin@orange.fr>
2022-04-13 19:09:45 +0200

    initialisation

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
+import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "./components/Home/Home";
+import Contact from "./components/Contact/Contact";
+import "./App.css";
 
 function App() {
   return (
@@ -7,6 +10,7 @@ function App() {
       <header className="App-header">Template Front-End</header>
       <Routes>
         <Route path="/" element={<Home />}></Route>
+        <Route path="contact" element={<Contact />}></Route>
       </Routes>
     </div>
   );
